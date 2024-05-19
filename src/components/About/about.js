788a3442edcafcafe7f9f1d6c81dc9f67a79520f
amberc15 <ambercarbajal33@gmail.com>
2024-05-19 16:33:51 -0700

    Favicon

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -3,12 +3,24 @@ import { Link } from 'react-router-dom';
 
 function About() {
   return (
-    <div>
-      <section>
-        <h2>Welcome to ReNewCycle Project</h2>
-      </section>
-    </div>
+    <>
+      <div>
+        <section>
+          <h2>Welcome to ReNewCycle Project</h2>
+        </section>
+      </div>
+      <div className="about-us">
+        <h2>About Us</h2>
+        <p>
+          Our team, comprised of students from the University of Washington, is dedicated to informing UW students about recycling.
+        </p>
+        <p>
+          Within our site, users will discover an array of tools such as a quiz and flashcards to enahnce their recycling knowledge.
+        </p>
+      </div>
+    </>
   );
 }
 
 export default About;
+
